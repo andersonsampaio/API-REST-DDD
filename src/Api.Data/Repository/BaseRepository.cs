324758b1c8be3f53cb6741419b699e839b6737fb
Anderson Sampaio <andersonsampaio@yahoo.com>
2023-03-27 19:44:18 -0300

    Api.Data - implementando as Entidades e Repositório base(Parte 3)

--- src/Api.Data/Repository/BaseRepository.cs
+++ src/Api.Data/Repository/BaseRepository.cs
@@ -1,7 +1,113 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Api.Data.Context;
+using Api.Domain.Entities;
+using Api.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
 namespace Api.Data.Repository
 {
-    public class BaseRepository
+    public class BaseRepository<T> : IRepository<T> where T : BaseEntity
     {
-        
+        protected readonly MyContext _context;
+        private DbSet<T> _dataBase;
+
+        public BaseRepository(MyContext context)
+        {
+            _context = context;
+            _dataBase = _context.Set<T>();
+        }
+        public async Task<bool> DeleteAsync(Guid id)
+        {
+            try
+            {
+                var result = await _dataBase.SingleOrDefaultAsync(p => p.Id.Equals(id));
+                if (result == null)
+                    return false;
+
+                _dataBase.Remove(result);
+                await _context.SaveChangesAsync();
+                return true;
+
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<T> InsertAsync(T item)
+        {
+            try
+            {
+                if (item.Id == Guid.Empty)
+                {
+                    item.Id = Guid.NewGuid();
+                }
+
+                item.CreatAt = DateTime.UtcNow;
+                _dataBase.Add(item);
+
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return item;
+        }
+
+        public async Task<bool> ExistAsync(Guid id)
+        {
+            return await _dataBase.AnyAsync(p => p.Id.Equals(id));
+        }
+
+        public async Task<T> SelectAsync(Guid id)
+        {
+            try
+            {
+                return await _dataBase.SingleOrDefaultAsync(p => p.Id.Equals(id));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<IEnumerable<T>> SelectAsync()
+        {
+            try
+            {
+                return await _dataBase.ToListAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<T> UpdateAsync(T item)
+        {
+            try
+            {
+                var result = await _dataBase.SingleOrDefaultAsync(p => p.Id.Equals(item.Id));
+                if (result == null)
+                    return null;
+
+                item.UpdateAt = DateTime.UtcNow;
+                item.CreatAt = result.CreatAt;
+
+                _context.Entry(result).CurrentValues.SetValues(item);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return item;
+        }
     }
 }
